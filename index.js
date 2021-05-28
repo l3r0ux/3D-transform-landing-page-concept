4f82756c8dc1f1f146724f8ce9620f4c4859711d
l3r0ux <lerouxvermeulen@gmail.com>
2021-05-28 21:20:05 +0200

    add back fading effect on scroll

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ window.addEventListener('scroll', () => {
     // If bottom of landing page is still not at the top of the page, send transform degrees to css
     if (landingPage.getBoundingClientRect().bottom > 0) {
         scrollPos.style.setProperty('--transform', `${parseFloat(90 - landingPage.getBoundingClientRect().bottom / window.innerHeight * 90)}deg`)
-        // landingPage.style.opacity = `${landingPage.getBoundingClientRect().bottom / window.innerHeight}`
-        // homePage.style.opacity = `${1 - landingPage.getBoundingClientRect().bottom / window.innerHeight}`
+        landingPage.style.opacity = `${landingPage.getBoundingClientRect().bottom / window.innerHeight}`
+        homePage.style.opacity = `${1 - landingPage.getBoundingClientRect().bottom / window.innerHeight}`
     }
     // if bottom of landing page is higher than top of screen, set degrees to 90 incase it couldnt update quick enough
     else {
         scrollPos.style.setProperty('--transform', '90deg')
-        // landingPage.style.opacity = '1';
-        // homePage.style.opacity = '1';
+        landingPage.style.opacity = '1';
+        homePage.style.opacity = '1';
     }
 })
